c2e2aad588a0abcb3d66274e949432b39ab338d4
wouldgo <wouldgo84@gmail.com>
2015-03-21 23:16:04 +0100

    new index going on

--- index.js
+++ index.js
@@ -5,7 +5,34 @@
   var mysql = require('mysql')
     , dataTypes = require('atthis-cm').dataTypes
     , MysqlDumper = function MysqlDumper() {
-      };
+      }
+    , connectionInformations = {
+      'menus': [
+        {
+          'host': dataTypes.string.required,
+          'port': dataTypes.number,
+          'username': dataTypes.string.required,
+          'password': dataTypes.password.required,
+          'ssl': dataTypes.checkbox
+        },
+        {
+          'socketPath': dataTypes.string.required,
+          'username': dataTypes.string.required,
+          'password': dataTypes.password.required,
+          'ssl': dataTypes.checkbox
+        }
+      ],
+      'options': {
+        'ssl': {
+          'keyfile': dataTypes.file.required,
+          'certificate': dataTypes.file.required,
+          'ca': dataTypes.file.required
+        }
+      },
+      'defaults': {
+        'port': 3306
+      }
+    };
 
   MysqlDumper.prototype.getName = function getName() {
 
@@ -14,27 +41,70 @@
 
   MysqlDumper.prototype.getConnectionInformations = function getConnectionInformations() {
 
-    return {
-      'host': dataTypes.string,
-      'port': dataTypes.number,
-      'user': dataTypes.string,
-      'password': dataTypes.password
+    return connectionInformations;
+  };
+
+  MysqlDumper.prototype.testConnectionInformations = function testConnectionInformations(conn) {
+
+    /*{
+      'host': '127.0.0.1',
+      'port': 3306,
+      'username': 'antani',
+      'password': 'antani',
+      'ssl': true,
+
+      'keyfile': '/home/antani/key.key',
+      'certificate': '/home/antani/cert.cert',
+      'ca': '/home/antani/ca.ca'
+    },
+    {
+      'socketPath': '/var/run/mysqld/socketPath.so',
+      'username': 'antani',
+      'password': 'antani',
+      'ssl': false
+    }*/
+
+    if (!conn) {
+
+      throw 'missing connection parameters';
+    }
+
+    var menusInformationsKeys = Object.keys(connectionInformations.menus)
+      , optionsInforationsKeys = Object.keys(connectionInformations.options)
+      , menusInformationsKeysLength = menusInformationsKeys.length
+      , optionsInforationsKeysLength = optionsInforationsKeys.length
+      , anIndex = 0
+      , aConnectionInformationsKey;
+    for (; anIndex < menusInformationsKeysLength; anIndex += 1) {
+
+      aConnectionInformationsKey = menusInformationsKeys[anIndex];
+      if (conn[aConnectionInformationsKey] === undefined) {
+
+        throw 'a required parameter is missing ' + aConnectionInformationsKey;
+      }
+    }
+
+    for (anIndex = 0; i < optionsInforationsKeysLength; anIndex += 1) {
+      Things[i]
     };
-  };
 
-  MysqlDumper.prototype.getDumpableResources = function getDumpableResources() {
 
-    return [];
-  };
-
-  MysqlDumper.prototype.testConnectionInformations = function testConnectionInformations() {
-
+    var connection = mysql.createConnection({
+      host: 'localhost',
+      user: 'me',
+      password: 'secret'
+    });
     return true;
   };
 
   MysqlDumper.prototype.doConnect = function doConnect() {
 
     return true;
+  };
+
+  MysqlDumper.prototype.getDumpableResources = function getDumpableResources() {
+
+    return [];
   };
 
   MysqlDumper.prototype.doDump = function doDump(informations) {
